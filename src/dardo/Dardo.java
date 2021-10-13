6a7e4464232ba1e0c1035e859484554f6ba3b7d7
K3vinchaparro <collegek450@gmail.com>
2021-10-12 23:38:50 -0500

    EXEEEE

--- src/dardo/Dardo.java
+++ src/dardo/Dardo.java
@@ -1,7 +1,9 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor. se volvio a una version anterior,
+eso no significa que vuelvas con tu ex
+
  */
 package dardo;
 
